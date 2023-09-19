5725afcad4f6e151d156aa23f4fc77de01634c81
Declan O'Sullivan <114866971+HawkEyeSolutions@users.noreply.github.com>
2023-09-19 15:37:17 +0000

    Update ruby and gems

--- static/js/index.js
+++
@@ -1,4 +0,0 @@
-const crypto = require('crypto');
-const secretKey = 'SECRET_KEY'; // secret key (keep safe!)
-const userIdentifier = current_user.id.toString(); // user's id
-const hash = crypto.createHmac('sha256', secretKey).update(userIdentifier).digest('hex');
